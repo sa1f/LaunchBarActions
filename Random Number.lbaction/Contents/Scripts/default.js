be4082b962507145eee499876797208b99c80fac
Saif Sajid <saifmsajid@gmail.com>
2015-12-13 12:39:48 -0800

    added default.js

---
+++ Random Number.lbaction/Contents/Scripts/default.js
@@ -0,0 +1,36 @@
+// LaunchBar Action Script
+
+function runWithString(string) {
+    if (string == undefined) {
+        // Inform the user that there was no argument
+        LaunchBar.alert("Hey I'll need two numbers separated by a space");
+    }
+		else {
+				var parts = string.split(" ");
+
+				var result = HTTP.postJSON('https://api.random.org/json-rpc/1/invoke', {
+						body: {
+							"jsonrpc": "2.0",
+							"method": "generateIntegers",
+							"params": {
+									"apiKey": "############INSERT RANDOM.ORG API KEY HERE##############",
+									"n": 1,
+									"min": Math.min.apply(null, parts),
+									"max": Math.max.apply(null, parts),
+									"replacement": true
+							},
+							"id": 42
+
+						}
+				});
+
+				var data = JSON.parse(result.data)
+				var randNumber = data.result.random.data[0];
+
+				LaunchBar.displayInLargeType({
+					title: "Your random number is",
+					string: randNumber
+				})
+
+    }
+}
